cd738f3986123b752da029d82fc812bef295b6c0
Shantinath <patilsammed188188@gmail.com>
2025-10-27 22:06:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+
+
+# to import the data
+df = pd.read_csv('india.csv')
+list_of_states = list(df['State'].unique())
+list_of_states.insert(0,'Overall India')
+
+
+
+st.sidebar.title('India map')
+selected_state = st.sidebar.selectbox('Select state',list_of_states)
+primary = st.sidebar.selectbox('Select primary parameter',sorted(df.columns[9:]))
+secondary = st.sidebar.selectbox('Select secondary parameter',sorted(df.columns[9:]))
+
+
+plot = st.sidebar.button('Plot Graph')
+
+if plot:
+    st.text('Size represents primary paramater')
+    st.text('Color represents secondary paramater')
+
+    # to plot for overall india
+    if selected_state == 'Overall India':
+        fig = px.scatter_map(df, lat='Latitude', lon='Longitude', zoom=3.5, map_style="carto-positron",size=primary,color=secondary,width=1200,height=600,hover_name='District')
+        # to plot plotly in streamlit
+        st.plotly_chart(fig,use_container_width=True)
+    else:
+        # plot for selected state
+        state_df = df[df['State'] == selected_state]
+        fig = px.scatter_map(state_df, lat='Latitude', lon='Longitude', zoom=5.5, map_style="carto-positron", size=primary,
+                             color=secondary, width=1200, height=600,hover_name="District")
+        # to plot plotly in streamlit
+        st.plotly_chart(fig, use_container_width=True)
